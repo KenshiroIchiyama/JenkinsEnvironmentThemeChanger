ab7ec08bc0baee0cda071f763730c0112cde8455
ichiyamak <ichiyamak@arzest.jp>
2019-02-22 11:43:09 +0900

    色がヤバかったので修正

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 var DEFAULT_HEADER_COLOR = '#000000';
-var DEV_HEADER_COLOR     = '#00FF00';
-var STAGING_HEADER_COLOR = '#0000FF';
+var DEV_HEADER_COLOR     = '#01DF3A';
+var STAGING_HEADER_COLOR = '#2E2EFE';
 var PROD_HEADER_COLOR    = '#FF0000';
 
 window.addEventListener("load", function()
